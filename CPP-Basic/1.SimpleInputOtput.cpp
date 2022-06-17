80e579687e45e68fdb13243d2a6e78eebf9589ad
Asif Bhuiyan Shawon <90594450+AsifShawon@users.noreply.github.com>
2022-06-17 21:02:11 +0600

    Add files via upload

---
+++ CPP-Basic/1.SimpleInputOtput.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    /*
+    "<<" = Insertion Operator / Printing operator
+    ">>" = Extraction Operator / Scanning Operator
+    */
+
+    cout<<"The Sum is: "<<num1+num2;
+
+    return 0;
+}
