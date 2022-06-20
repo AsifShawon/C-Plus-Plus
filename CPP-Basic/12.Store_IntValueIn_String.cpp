07bc747cc2d4c8ad5114d8016664319d41b9e744
Asif Bhuiyan Shawon <90594450+AsifShawon@users.noreply.github.com>
2022-06-21 01:31:33 +0600

    Add files via upload

---
+++ CPP-Basic/12.Store_IntValueIn_String.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //int element
+    int a = 12345;
+    cout<<"Value in a: "<<a<<endl;
+    //store a's value in a string
+    string str = to_string(a); // "to_string(value)" is a string function to store a non character value in string;
+    cout<<"After storing a's value in str: "<<str<<endl;
+}
+
