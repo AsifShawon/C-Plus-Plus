9fdfc3a6b3b6b1db77ac48c50861b91869b5d981
Asif Bhuiyan Shawon <asifbhshawon71@gmail.com>
2022-11-29 15:01:28 +0600

    Add files via upload

---
+++ Lab/Lab08_Stack_Array/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include "stacktype.h"
+#include "stacktype.cpp"
+#include<string>
+using namespace std;
+
+int main()
+{
+    // Create a stack of integers
+    StackType<int> st;
+
+    //  Check if the stack is empty
+    if(st.IsEmpty()) cout << "Stack is empty\n";
+    else cout << "Stack is not empty\n";
+
+    // Push four items
+    st.Push(5);
+    st.Push(7);
+    st.Push(4);
+    st.Push(2);
+
+    // Check if the stack is empty
+    if(st.IsEmpty()) cout << "Stack is empty\n";
+    else cout << "Stack is not empty\n";
+
+    // Check if the stack is full
+    if(st.IsFull()) cout << "Stack is full\n";
+    cout << "Stack is not full\n";
+
+    // Print the values in the stack (in the order the values are given as input)
+    StackType<int> st2;
+
+    while(!st.IsEmpty()){
+        st2.Push(st.Top());
+        st.Pop();
+    }
+
+    while(!st2.IsEmpty()){
+        cout << st2.Top() << " ";
+        st.Push(st2.Top());
+        st2.Pop();
+    }
+
+    cout << endl;
+
+    // Push another item
+    st.Push(3);
+
+    // Print the values in the stack
+    while(!st.IsEmpty()){
+        st2.Push(st.Top());
+        st.Pop();
+    }
+
+    while(!st2.IsEmpty()){
+        cout << st2.Top() << " ";
+        st.Push(st2.Top());
+        st2.Pop();
+    }
+
+    cout << endl;
+
+    // check if the stack is full
+    if(st.IsFull()) cout << "Stack is full\n";
+    cout << "Stack is not full\n";
+
+    // pop two items
+    st.Pop();
+    st.Pop();
+
+    // print top items
+    cout << st.Top() << endl;
+
+
+    // task 2
+    StackType<char> char_stack;
+    string s;
+    cin >> s;
+
+    if(char_stack.isBalanace(s)) cout << "Balanced\n";
+    else cout << "Not Balanced\n";
+
+    return 0;
+}
---
+++ Lab/Lab08_Stack_Array/stacktype.cpp
@@ -0,0 +1,62 @@
+#include "StackType.h"
+template <class ItemType>
+StackType<ItemType>::StackType()
+{
+    top = -1;
+}
+template <class ItemType>
+bool StackType<ItemType>::IsEmpty()
+{
+    return (top == -1);
+}
+template <class ItemType>
+bool StackType<ItemType>::IsFull()
+{
+    return (top == MAX_ITEMS-1);
+}
+template <class ItemType>
+void StackType<ItemType>::Push(ItemType newItem)
+{
+    if( IsFull() ) throw FullStack();
+    top++;
+    items[top] = newItem;
+}
+template <class ItemType>
+void StackType<ItemType>::Pop()
+{
+    if( IsEmpty() ) throw EmptyStack();
+    top--;
+}
+template <class ItemType>
+ItemType StackType<ItemType>::Top()
+{
+    if (IsEmpty()) throw EmptyStack();
+    return items[top];
+}
+
+// to check bracket balance
+template <class ItemType>
+bool StackType<ItemType>::isBalanace(string s)
+{
+    StackType<char> br;
+    bool flag = true;
+
+    /*** solution: if we have a opening bracket then we have to have a closing bracket.
+                   but if the first one is closing bracket then not balanced.*/
+
+    for(int i=0; i<s.size(); i++){
+        if(s[i]=='(') br.Push(s[i]);
+        else{
+            if(s[i]==')')
+                if(br.IsEmpty() || br.Top()!='('){
+                    flag = false;
+                    break;
+                }
+               else br.Pop();
+        }
+    }
+
+    if(br.IsEmpty() && flag) flag = true;
+    else flag = false;
+    return flag;
+}
